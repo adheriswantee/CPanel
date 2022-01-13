f20b4f88670667d6047d70ee6d843f8d07965d51
Julio <59321246+JulioCodes@users.noreply.github.com>
2022-01-12 20:59:16 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const menuToggleOpen = document.querySelector('.menu-toggle-open');
+const menuToggleClose = document.querySelector('.menu-toggle-close');
+const navMobileElement = document.querySelector('.nav-mobile');
+
+menuToggleOpen.addEventListener('click', () => {
+    navMobileElement.classList.add('active');
+});
+
+menuToggleClose.addEventListener('click', () => {
+    navMobileElement.classList.remove('active');
+});
